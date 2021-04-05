233f0f6e7c040b2e95847f196d10f12230e1f78e
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2021-04-05 11:37:18 -0500

    Sede done

--- components/sede/sede.tsx
+++ components/sede/sede.tsx
@@ -1,11 +1,17 @@
+import styles from "./style.module.css"
 export interface SedeProps {
     
 }
  
 const Sede: React.SFC<SedeProps> = () => {
     return (
-        <div>
-            
+        <div className={styles.body_sede_container}>
+            <h2>Sede</h2>
+            <div className={styles.body_sede_info_container}>
+                <view className={styles.body_sede_img}/>
+                <h3>SOS Medical Group Clinic</h3>
+                <p>SOS clinic with modern medical technology infrastructure and efficient procedures fully complemented with a doctor and highly qualified staff care</p>
+            </div>
         </div>
     );
 }
